45ec088908b38bc011bf364f2ee62f5341a959d5
dickus <dickie.dickus@proton.me>
2025-03-25 13:13:14 +0300

    wallpapers script update

--- .config/scripts/wallpaper_changer.sh
+++ .config/scripts/wallpaper_changer.sh
@@ -2,5 +2,15 @@
 
 export DISPLAY=:0.0
 
-feh --bg-fill --randomize ~/Pictures/wallpapers/
+current_hour=$(date +%H)
+current_minute=$(date +%M)
+total_minutes=$((10#$current_hour * 60 + 10#$current_minute))
+
+if (( total_minutes >= 480 && total_minutes < 1170 )); then
+    mode="light"
+else
+    mode="dark"
+fi
+
+feh --bg-fill --randomize ~/Pictures/wallpapers/$mode
 
