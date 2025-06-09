2a9db444fbda55bb22280304e7b0ca2f49df84d9
dickus <dickie.dickus@proton.me>
2025-06-09 21:12:18 +0300

    feat: now it shows days without incidents

--- .config/scripts/creation_time.sh
+++ .config/scripts/creation_time.sh
@@ -3,9 +3,23 @@
 CREATION_DATE=$(stat -c %W /)
 CURRENT_TIME=$(date +%s)
 TIME_SINCE_CREATED=$(($CURRENT_TIME - $CREATION_DATE))
+
 DAYS=$(($TIME_SINCE_CREATED / 86400))
 HOURS=$((($TIME_SINCE_CREATED % 86400) / 3600))
-MINUTES=$((($TIME_SINCE_CREATED % 3600) / 60))
 
-echo "${DAYS}d ${HOURS}h ${MINUTES}m"
+if [[ -f "$HOME/.config/scripts/fuck" ]]; then
+    INCIDENT_DATE=$(stat -c %W "$HOME/.config/scripts/fuck")
+    TIME_WITHOUT_INCIDENT=$(($CURRENT_TIME - $INCIDENT_DATE))
+else
+    TIME_WITHOUT_INCIDENT=$(($CURRENT_TIME - $CREATION_DATE))
+fi
+
+DAYS_WITHOUT_INCIDENT=$(($TIME_WITHOUT_INCIDENT / 86400))
+
+result=""
+[[ $DAYS -gt 0 ]] && result="${result}${result:+ }${DAYS}d"
+[[ $HOURS -gt 0 ]] && result="${result}${result:+ }${HOURS}h"
+result="${result}${result:+}, days without incidents: ${DAYS_WITHOUT_INCIDENT}"
+
+echo "$result"
 
