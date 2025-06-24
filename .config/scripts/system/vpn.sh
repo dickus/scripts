66ed9282429232f027849e05b53651e50054d968
dickus <dickie.dickus@proton.me>
2025-06-24 10:20:44 +0300

    feat: full vpn.sh rework for rofi

--- .config/scripts/system/vpn.sh
+++ .config/scripts/system/vpn.sh
@@ -1,20 +1,59 @@
 #!/bin/bash
 
-CONNECTION=$(ifconfig | grep "vpn")
+BASE_DIR="$HOME/.wg"
 
-if [[ -z "$CONNECTION" ]]; then
-    wg-quick up .wg/vpn.conf
+up() {
+    CONNECTION=$(ifconfig | grep "vpn")
 
+    ! [[ -z "$CONNECTION" ]] && dunstify -u critical -t 3000 -i $HOME/.icons/dark/connection_down.svg "VPN" "Already connected" && exit 1
+
+    existing_files=()
+    if [[ -d "$BASE_DIR" ]]; then
+        while IFS= read -r file; do
+            existing_files+=("$file")
+        done < <(find "$BASE_DIR" -maxdepth 1 -name "*.conf" -type f -printf "%f\n")
+    fi
+
+    filename=$(printf "%s\n" "${existing_files[@]}" | rofi -dmenu \
+        -p "VPN config:" \
+        -theme-str "window { width: 20%; }" \
+        -theme-str "listview { lines: 5; columns: 2; }"
+    )
+
+    [[ -z "$filename" ]] && exit 0
+
+    wg-quick up ${BASE_DIR}/"$filename"
+
+    filename=$(echo "$filename" | sed 's|^/||; s|\.conf$||')
+
+    dunstify -t 1500 -i $HOME/.icons/dark/connection_up.svg "VPN" "Connected to ${filename}"
+}
+
+down() {
     CONNECTION=$(ifconfig | grep "vpn")
 
     if [[ -z "$CONNECTION" ]]; then
-        dunstify -t 2000 -i $HOME/.icons/dark/connection_down.svg "VPN" "Connection error"
+        dunstify -u critical -t 3000 -i $HOME/.icons/dark/connection_down.svg "VPN" "Connection error"
     else
-        dunstify -t 1500 -i $HOME/.icons/dark/connection_up.svg "VPN" "Connection up"
-    fi
-else
-    wg-quick down .wg/vpn.conf
+        for conf_file in "$BASE_DIR"/*.conf; do
+            [[ -e "$conf_file" ]] || continue
 
-    dunstify -t 1500 -i $HOME/.icons/dark/connection_down.svg "VPN" "Connection down"
-fi
+            interface_name=$(basename "$conf_file")
+
+            wg-quick down ${BASE_DIR}/"$interface_name"
+        done
+
+        dunstify -t 1500 -i $HOME/.icons/dark/connection_down.svg "VPN" "Connection down"
+    fi
+}
+
+mode=$(echo -e "up\ndown" | rofi -dmenu \
+    -p "Mode:" \
+    -theme-str "window { width: 7%; }" \
+    -theme-str "listview { lines: 2; }"
+)
+
+[[ -z "$mode" ]] && exit 0
+
+$mode
 
