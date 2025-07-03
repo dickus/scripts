7ea31525a22a3985a0dbbaa4bb587421d18e4728
dickus <dickie.dickus@proton.me>
2025-07-03 15:16:27 +0300

    feat: updated for hyprland

--- .config/scripts/system/screenshoter.sh
+++ .config/scripts/system/screenshoter.sh
@@ -9,8 +9,15 @@ if [[ "$XDG_SESSION_TYPE" = "wayland" ]]; then
     case "$1" in
         screen)
             grim "$SCREEN_DIR/$FILE" && $CLIP < "$SCREEN_DIR/$FILE" ;;
+
         region)
             grim -g "$(slurp)" "$SCREEN_DIR/$FILE" && $CLIP < "$SCREEN_DIR/$FILE" ;;
+
+        window)
+            AT=$(hyprctl activewindow | grep "at: [0-9]" | sed "s|[[:space:]]*at: ||")
+            SIZE=$(hyprctl activewindow | grep "size: [0-9]" | sed "s|[[:space:]]*size: ||; s|,|x|")
+
+            grim -g "$AT $SIZE" "$SCREEN_DIR/$FILE" && $CLIP < "$SCREEN_DIR/$FILE" ;;
     esac
 else
     CLIP="xclip -sel clip -t image/png -i"
