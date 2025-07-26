33bcdbb36ff951e696c24b3b004ef80991b987e2
dickus <dickie.dickus@proton.me>
2025-07-26 14:36:55 +0300

    feat: new_note.sh to rofi

--- .config/scripts/rofi.sh
+++ .config/scripts/rofi.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 SCRIPTS_DIR="$HOME/.config/scripts/rofi_scripts"
+TERMINAL="kitty"
 
-script=$(echo -e "Wallpaper\nVPN\nYouTube" | rofi -dmenu \
+script=$(echo -e "Wallpaper\nNote\nVPN\nYouTube" | rofi -dmenu \
     -p "Script:" \
     -theme-str "window { width: 15%; }" \
     -theme-str "listview { lines: 5; }"
@@ -11,6 +12,8 @@ script=$(echo -e "Wallpaper\nVPN\nYouTube" | rofi -dmenu \
 case $script in
     Wallpaper)
         ${SCRIPTS_DIR}/new_wallpaper.sh ;;
+    Note)
+        ${SCRIPTS_DIR}/new_note.sh ${TERMINAL};;
     VPN)
         ${SCRIPTS_DIR}/vpn.sh ;;
     YouTube)
