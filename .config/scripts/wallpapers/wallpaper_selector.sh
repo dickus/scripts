4e58594e61d2695945cf05438b8bff7d5bcce149
dickus <dickie.dickus@proton.me>
2025-08-08 15:10:24 +0300

    fix: cleanup

--- .config/scripts/wallpapers/wallpaper_selector.sh
+++ .config/scripts/wallpapers/wallpaper_selector.sh
@@ -10,28 +10,35 @@ if [[ -d "${WALL_DIR}" ]]; then
     done < <(find "${WALL_DIR}" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
 fi
 
-category=$(printf "%s\n" "${categories[@]}" | sort | rofi -dmenu \
+category=$(printf "%s\n" "${categories[@]}" | \
+    sort | \
+    rofi -dmenu \
     -p "Wallpapers:" \
     -i \
-    -theme-str "window { width: 10%; }" \
-    -theme-str "listview { lines: 2; }"
+    -theme-str "window { width: 8%; }" \
+    -theme-str "listview { lines: 5; columns: $(printf "%s\n" ${categories[@]} | \
+        wc -l); }"
 )
 
 [[ -z "${category}" ]] && exit 0
 
 manual() {
-    find ${WALL_DIR}/${category} -type f | sort | xargs -r swayimg -g
+    find ${WALL_DIR}/${category} -type f | \
+        sort | \
+        xargs -r swayimg -g
 }
 
 random() {
     mapfile -t files < <(find ${WALL_DIR}/${category} -type f | \
         sort)
 
-    files_count=$(printf "%s\n" "${files[@]}" | wc -l)
+    files_count=$(printf "%s\n" "${files[@]}" | \
+        wc -l)
 
     file=$((1 + RANDOM % ${files_count}))
 
-    ${HOME}/.config/scripts/wallpapers/wall_set.sh "$(printf "%s\n" "${files[@]}" | sed -n "${file}p")"
+    ${HOME}/.config/scripts/wallpapers/wall_set.sh "$(printf "%s\n" "${files[@]}" | \
+        sed -n "${file}p")"
 }
 
 if [[ "${MODE}" == "random" ]]; then
