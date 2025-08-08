cd98dfa1202ef9345ffaa72aa4ddd6e48118b750
dickus <dickie.dickus@proton.me>
2025-08-08 15:03:36 +0300

    feat: run TSM, wowup and then battle.net

--- .config/scripts/apps.sh
+++ .config/scripts/apps.sh
@@ -26,6 +26,10 @@ elif [[ "${app}" == "Audacity" ]]; then
 elif [[ "${app}" == "Steam" ]]; then
     mangohud steam -console
 elif [[ "${app}" == "Battle.net" ]]; then
+    env LUTRIS_SKIP_INIT=1 lutris lutris:rungameid/1 &
+
+    wowup-cf
+
     env "$HOME/PortProton/data/scripts/start.sh" "$HOME/PortProton/data/prefixes/BATTLE_NET/drive_c/Program Files (x86)/Battle.net/Battle.net.exe"
 fi
 
