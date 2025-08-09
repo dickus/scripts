22acef7c1ecb46ca8c7f503a3b31d3398fb79ba4
dickus <dickie.dickus@proton.me>
2025-08-09 17:56:20 +0300

    feat: cleanup and wallpaper setting based on theme

--- .config/scripts/theme_change/theme_schedule.sh
+++ .config/scripts/theme_change/theme_schedule.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 THEME="$1"
-LIGHT_THEME="latte"
-DARK_THEME="frappe"
+LIGHT_THEME="fjord"
+DARK_THEME="nord"
 
-CONFIG_DIR="$HOME/.config"
+CONFIG_DIR="${HOME}/.config"
+WALL_DIR="${HOME}/Pictures/wallpapers"
 
-TARGET_THEME="$LIGHT_THEME"
+TARGET_THEME="${LIGHT_THEME}"
 DIR_MODE="light"
+TERMINAL="kitty"
 
-if [[ "$THEME" == "dark" ]]; then
-    TARGET_THEME="$DARK_THEME"
+if [[ "${THEME}" == "dark" ]]; then
+    TARGET_THEME="${DARK_THEME}"
     DIR_MODE="dark"
 fi
 
 declare -A replacements=(
-    ["$CONFIG_DIR/bspwm/bspwmrc"]="s|/themes/.*.sh|/themes/$TARGET_THEME.sh|"
-    ["$CONFIG_DIR/hypr/hyprland.conf"]="s|/dunstrc_.*|/dunstrc_$TARGET_THEME| ; s|/themes/.*.conf|/themes/$TARGET_THEME.conf|"
-    ["$CONFIG_DIR/hypr/hyprlock.conf"]="s|themes/.*.conf|themes/$TARGET_THEME.conf|"
-    ["$CONFIG_DIR/nvim/init.lua"]="s|themes.*|themes.$TARGET_THEME\")|"
-    ["$CONFIG_DIR/polybar/config.ini"]="s|/themes/.*.ini|/themes/$TARGET_THEME.ini|"
-    ["$CONFIG_DIR/rofi/config.rasi"]="s|/themes/.*.rasi|/themes/$TARGET_THEME.rasi|"
-    ["$CONFIG_DIR/rofi/powermenu.rasi"]="s|/themes/.*.rasi|/themes/$TARGET_THEME.rasi|"
+    ["${CONFIG_DIR}/hypr/hyprland.conf"]="s|/dunstrc_.*|/dunstrc_${TARGET_THEME}| ; s|/themes/.*.conf|/themes/${TARGET_THEME}.conf|"
+    ["${CONFIG_DIR}/hypr/hyprlock.conf"]="s|themes/.*.conf|themes/${TARGET_THEME}.conf|"
+    ["${CONFIG_DIR}/nvim/init.lua"]="s|themes.*|themes.${TARGET_THEME}\")|"
+    ["${CONFIG_DIR}/rofi/config.rasi"]="s|/themes/.*.rasi|/themes/${TARGET_THEME}.rasi|"
+    ["${CONFIG_DIR}/rofi/powermenu.rasi"]="s|/themes/.*.rasi|/themes/${TARGET_THEME}.rasi|"
 )
 
 for file in "${!replacements[@]}"; do
-    if [[ -f "$file" ]]; then
-        sed -i "${replacements[$file]}" "$file"
+    if [[ -f "${file}" ]]; then
+        sed -i "${replacements[${file}]}" "${file}"
     fi
 done
 
@@ -40,21 +40,42 @@ scripts=(
 )
 
 for script in "${scripts[@]}"; do
-    file="$CONFIG_DIR/scripts/$script"
+    file="${CONFIG_DIR}/scripts/${script}"
 
-    if [[ -f "$file" ]]; then
-        sed -i "s|/light/|/$DIR_MODE/|g; s|/dark/|/$DIR_MODE/|g" "$file"
+    if [[ -f "${file}" ]]; then
+        sed -i "s|/light/|/${DIR_MODE}/|g; s|/dark/|/${DIR_MODE}/|g" "${file}"
     fi
 done
 
-cat "$CONFIG_DIR/waybar/themes/$TARGET_THEME.css" > "$CONFIG_DIR/waybar/style.css"
-cat "$CONFIG_DIR/swayimg/themes/$TARGET_THEME" > "$CONFIG_DIR/swayimg/config"
+cat "${CONFIG_DIR}/waybar/themes/${TARGET_THEME}.css" > "${CONFIG_DIR}/waybar/style.css"
+cat "${CONFIG_DIR}/swayimg/themes/${TARGET_THEME}" > "${CONFIG_DIR}/swayimg/config"
 
 hyprctl reload
 
-if [[ "$DIR_MODE" == "light" ]]; then
-    kitty +kitten themes --reload-in=all "${LIGHT_THEME^}"
-else
-    kitty +kitten themes --reload-in=all "${DARK_THEME^}"
+if [[ "${TERMINAL}" == "kitty" ]]; then
+    if [[ "${DIR_MODE}" == "light" ]]; then
+        kitty +kitten themes --reload-in=all "${LIGHT_THEME^}"
+
+        mapfile -t files < <(find ${WALL_DIR}/${LIGHT_THEME} -type f | \
+            sort)
+
+        files_count=$(printf "%s\n" "${files[@]}" | \
+            wc -l)
+
+        file=$((1 + RANDOM % ${files_count}))
+    else
+        kitty +kitten themes --reload-in=all "${DARK_THEME^}"
+
+        mapfile -t files < <(find ${WALL_DIR}/${DARK_THEME} -type f | \
+            sort)
+
+        files_count=$(printf "%s\n" "${files[@]}" | \
+            wc -l)
+
+        file=$((1 + RANDOM % ${files_count}))
+    fi
 fi
+
+${HOME}/.config/scripts/wallpapers/wall_set.sh "$(printf "%s\n" "${files[@]}" | \
+    sed -n "${file}p")"
 
