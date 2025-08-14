ffb44fd769f38ff53b6713ddf9d50ba2d08eb93d
dickus <dickie.dickus@proton.me>
2025-08-14 10:02:23 +0300

    feat: cleanup and rng

--- .config/scripts/rofi.sh
+++ .config/scripts/rofi.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-SCRIPTS_DIR="$HOME/.config/scripts"
+SCRIPTS_DIR="${HOME}/.config/scripts"
 
-script=$(echo -e "New wallpaper\nVPN\nYouTube download" | rofi -dmenu \
+script=$(echo -e "New wallpaper\nVPN\nYouTube download\nRandom" | \
+    sort | \
+    rofi -dmenu \
     -p "Script:" \
     -i \
     -theme-str "window { width: 10%; }" \
-    -theme-str "listview { lines: 3; }"
+    -theme-str "listview { lines: 4; }"
 )
 
 [[ -z "${script}" ]] && exit 0
 
-if [[ "${script}" == "New wallpaper" ]]; then
-    ${SCRIPTS_DIR}/system/new_wallpaper.sh
-elif [[ "${script}" == "VPN" ]]; then
-    ${SCRIPTS_DIR}/system/vpn.sh
-elif [[ "${script}" == "YouTube download" ]]; then
-    ${SCRIPTS_DIR}/system/yt.sh
-fi
+case "${script}" in
+    "New wallpaper") ${SCRIPTS_DIR}/system/new_wallpaper.sh ;;
+    "VPN") ${SCRIPTS_DIR}/system/vpn.sh ;;
+    "YouTube download") ${SCRIPTS_DIR}/system/yt.sh ;;
+    "Random") ${SCRIPTS_DIR}/random.sh ;;
+esac
 
