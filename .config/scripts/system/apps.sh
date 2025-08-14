a846207affec61643446f696ca2cb2c58e8b8617
dickus <dickie.dickus@proton.me>
2025-08-14 10:01:26 +0300

    feat: moved to system scripts

--- .config/scripts/system/apps.sh
+++ .config/scripts/system/apps.sh
@@ -30,6 +30,6 @@ elif [[ "${app}" == "Battle.net" ]]; then
 
     wowup-cf
 
-    env "$HOME/PortProton/data/scripts/start.sh" "$HOME/PortProton/data/prefixes/BATTLE_NET/drive_c/Program Files (x86)/Battle.net/Battle.net.exe"
+    env "${HOME}/PortProton/data/scripts/start.sh" "${HOME}/PortProton/data/prefixes/BATTLE_NET/drive_c/Program Files (x86)/Battle.net/Battle.net.exe"
 fi
 
