8a2acadf37c4d8fe5dd8620a3516a03cf9850ef9
dickus <dickie.dickus@proton.me>
2025-09-18 16:57:36 +0300

    feat: refactor

---
+++ .config/scripts/system/daylight/daylight.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+TEMP=$(hyprctl hyprsunset temperature)
+
+DAY=""
+NIGHT="󰖔"
+
+THEME_SCRIPT="${HOME}/.config/scripts/theme_change/theme_schedule.sh"
+STATUS="${HOME}/.config/scripts/system/daylight/daylight"
+
+get_current_theme() {
+    grep -oP "${1}=\"\K[^\"]+" "${THEME_SCRIPT}" | tail -n 1
+}
+
+if [[ $(grep "6000" ${STATUS}) ]]; then
+    printf '{"text": "%s"}' "${DAY}"
+else
+    printf '{"text": "%s", "class": "on"}' "${NIGHT}"
+fi
+
+if [[ "${1}" == "toggle" ]]; then
+    if [[ -z $(grep "dark" ${HOME}/.config/scripts/system/microphone.sh) ]]; then
+        ${THEME_SCRIPT} dark
+    else
+        ${THEME_SCRIPT} light
+    fi
+fi
+
+if [[ "${1}" == "light" ]]; then
+    if [[ -z $(grep "6000" ${STATUS}) ]]; then
+        hyprctl hyprsunset identity
+
+        echo "6000" > ${STATUS}
+    else
+        hyprctl hyprsunset temperature 4500
+
+        echo "4500" > ${STATUS}
+    fi
+fi
+
