ca68f81d0f2ab24d6270d5f3ea8bf380e3ebf53d
dickus <dickie.dickus@proton.me>
2025-09-18 17:10:02 +0300

    feat: running garbage

---
+++ .config/scripts/run/zoom.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+XDG_SESSION_TYPE="" us.zoom.Zoom
+
---
+++ .config/scripts/system/garbage.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+garbage=(
+    "Yandex Browser"
+    "Zoom"
+)
+
+app=$(printf "%s\n" "${garbage[@]}" | \
+    sort | \
+    rofi -dmenu \
+    -p "Apps:" \
+    -i \
+    -theme-str "window { width: 10%; }" \
+    -theme-str "listview { lines: 2; }"
+)
+
+[[ -z "${app}" ]] && exit 0
+
+case "${app}" in
+    "Yandex Browser")
+        ru.yandex.Browser ;;
+    "Zoom")
+        ${HOME}/.config/scripts/run/zoom.sh ;;
+esac
+
