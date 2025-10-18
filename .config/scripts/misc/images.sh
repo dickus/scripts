7b1dadaff61d48dcf8e437ef74956804d7ff6812
dickus <dickie.dickus@proton.me>
2025-10-18 22:40:57 +0300

    feat: fzf script for removing, copying and opening in gimp screenshots

---
+++ .config/scripts/misc/images.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+IMAGES="${HOME}/Pictures"
+
+find "${IMAGES}" -maxdepth 1 -type f -iname "*.png" -printf "%f\n" | \
+    fzf \
+    --preview-window=right:65% \
+    --preview="kitty +kitten icat --clear --transfer-mode=memory --stdin=no --place=\${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 "${IMAGES}"/{}" \
+    --bind="ctrl-c:execute(wl-copy < "${IMAGES}"/{} &>/dev/null)+abort" \
+    --bind="ctrl-g:execute(setsid -f org.gimp.GIMP "${IMAGES}"/{} &>/dev/null &)+abort" \
+    --bind="ctrl-d:execute(mv "${IMAGES}"/{} "${HOME}"/.local/share/Trash)+reload(find "${IMAGES}" -maxdepth 1 -type f -iname \"*.png\" -printf \"%f\n\")" \
+    --bind="return:execute(setsid -f swayimg "${IMAGES}"/{} &>/dev/null)+abort"
+
