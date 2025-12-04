db46e8bdc42692d63ededa141b7e17f5ae3bcef3
dickus <dickie.dickus@proton.me>
2025-12-04 16:40:51 +0300

    fix: refactor

--- .config/scripts/misc/package_management.sh
+++ .config/scripts/misc/package_management.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MODE="${1}"
+MODE=""
 TERMINAL="kitty"
 
 pakmans=(
@@ -8,7 +8,29 @@ pakmans=(
     "flatpak"
 )
 
-package_manager() {
+actions=(
+    "install"
+    "remove"
+    "update"
+)
+
+choose_action() {
+    action=$(
+        printf "%s\n" "${actions[@]}" | \
+        rofi -dmenu \
+            -p "Choose action:" \
+            -i \
+            -theme-str "window { width: 10%; }" \
+            -theme-str "listview { lines: $( \
+                printf "%s\n" "${actions[@]}" | \
+                wc -l
+            ); }"
+        )
+
+    MODE=${action}
+}
+
+choose_package_manager() {
     pakman=$(
         printf "%s\n" "${pakmans[@]}" | \
         sort | \
@@ -24,18 +46,19 @@ package_manager() {
 }
 
 install() {
-    package_manager
+    choose_package_manager
 
     case "${pakman}" in
         "paru")
             ${TERMINAL} -T "pakman" -e bash -c '
             mapfile -t pkgs < <(
                 paru -Slq |
-                fzf --multi --preview "paru -Sii --needed {}" --preview-window=down:75%:wrap)
-                if (( ${#pkgs[@]} )); then
-                    paru -S "${pkgs[@]}"
-                    read -r -p "Press Enter to exit"
-                fi
+                fzf --multi --preview "paru -Sii --needed {}" --preview-window=down:75%:wrap
+            )
+            if (( ${#pkgs[@]} )); then
+                paru -S "${pkgs[@]}"
+                read -r -p "Press Enter to exit"
+            fi
             ' ;;
         "flatpak")
             ${TERMINAL} -T "pakman" -e bash -c '
@@ -43,34 +66,37 @@ install() {
                 flatpak remote-ls flathub --columns=application,branch |
                 awk "!x[\$1]++" |
                 fzf --multi --preview "flatpak remote-info flathub {1}/x86_64/{2}" --preview-window=down:75%:wrap --with-nth 1 |
-                awk "{print \$1}")
-                if (( ${#pkgs[@]} )); then
-                    flatpak install -y "${pkgs[@]}"
-                    read -r -p "Press Enter to exit"
-                fi
+                awk "{print \$1}"
+            )
+            if (( ${#pkgs[@]} )); then
+                flatpak install -y "${pkgs[@]}"
+                read -r -p "Press Enter to exit"
+            fi
             ' ;;
-        esac
+    esac
 }
 
-deinstall() {
-    package_manager
+remove() {
+    choose_package_manager
 
     case "${pakman}" in
         "paru")
             ${TERMINAL} -T "pakman" -e bash -c '
             mapfile -t pkgs < <(
                 paru -Qq |
-                fzf --multi --preview "paru -Sii {}" --preview-window=down:75%:wrap)
-                if (( ${#pkgs[@]} )); then
-                    paru -Rns "${pkgs[@]}"
-                    read -r -p "Press Enter to exit"
-                fi
+                fzf --multi --preview "paru -Sii {}" --preview-window=down:75%:wrap
+            )
+            if (( ${#pkgs[@]} )); then
+                paru -Rns "${pkgs[@]}"
+                read -r -p "Press Enter to exit"
+            fi
             ' ;;
         "flatpak")
             ${TERMINAL} -T "pakman" -e bash -c '
             mapfile -t pkgs < <(
                 flatpak list --app --columns=application |
-                fzf --multi --preview "flatpak info {}" --preview-window=down:75%:wrap)
+                fzf --multi --preview "flatpak info {}" --preview-window=down:75%:wrap
+            )
             if (( ${#pkgs[@]} )); then
                 flatpak uninstall -y "${pkgs[@]}"
                 read -r -p "Press Enter to exit"
@@ -79,12 +105,18 @@ deinstall() {
     esac
 }
 
+update() {
+    ${TERMINAL} -T "pakman" -e bash -c "paru -Syu; flatpak update"
+}
+
+choose_action
+
 case "${MODE}" in
     "install")
         install ;;
-    "deinstall")
-        deinstall ;;
+    "remove")
+        remove ;;
     "update")
-        ${TERMINAL} -T "pakman" -e bash -c "paru; flatpak update" ;;
+        update ;;
 esac
 
