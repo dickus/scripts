1b6c4b5bc60d98ba6d74b11aa9e4bcfea8d56d5e
dickus <dickie.dickus@proton.me>
2026-01-03 03:48:35 +0300

    feat: range instead of number

--- .config/scripts/misc/random.sh
+++ .config/scripts/misc/random.sh
@@ -1,19 +1,42 @@
 #!/usr/bin/env bash
 
-number=$(
+input=$(
     rofi -dmenu \
         -p "Integer:" \
         -theme-str "window { width: 15%; }" \
         -theme-str "listview { lines: 1; }" \
 )
 
-if ! [[ ${number} =~ ^[1-9][0-9]*$ ]]; then
-    notify-send -t 5000 "Are you retarded or something?"
+
+show_error() {
+    MSG="${1}"
+
+    notify-send -t 5000 "Error" "${MSG}"
 
     exit 0
-fi
+}
 
-RANDOM_NUMBER=$((1 + RANDOM % ${number}))
+
+if [[ ${input} == *"-"* ]]; then
+    IFS="-" read -r start end <<< "${input}"
+    
+    if ! [[ ${start} =~ ^[1-9][0-9]*$ && ${end} =~ ^[1-9][0-9]*$ ]]; then
+        show_error "Are you retarded or something?"
+    fi
+
+    if (( start >= end )); then
+        show_error "Start cannot be equal to end"
+    fi
+
+    range=$(( end - start + 1 ))
+    RANDOM_NUMBER=$(( start + RANDOM % range ))
+else
+    if ! [[ ${input} =~ ^[1-9][0-9]*$ ]]; then
+        error_exit
+    fi
+
+    RANDOM_NUMBER=$(( 1 + RANDOM % input ))
+fi
 
 notify-send -t 5000 "${RANDOM_NUMBER}"
 wl-copy "${RANDOM_NUMBER}"
